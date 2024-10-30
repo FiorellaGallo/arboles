e94bb4fec4854f7416521f223d88cc82a7608811
María Agustina Ortiz <95661814+agustina-ortiz@users.noreply.github.com>
2024-10-29 22:22:49 -0300

    ejercicio7

---
+++ ejercicio7.h
@@ -0,0 +1,11 @@
+#ifndef ejercicio7_h
+#define ejercicio7_h
+#include <stdio.h>
+#include "nodo.h"
+#include "arbol-binario.h"
+
+bool nodosEquivalentes(NodoArbol nodo1, NodoArbol nodo2);
+bool sonEquivalentes(ArbolBinario arbol1, ArbolBinario arbol2);
+
+#endif //ejercicio7_h
+
